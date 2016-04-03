d33e39f8494e524042621cdacd9dbb23c5c52975
AnObfuscator <anobfuscator@gmail.com>
2016-04-03 13:25:37 -0500

    Add functional testing script

---
+++ functional_tests/test_helpers.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import subprocess
+import psutil
+
+
+def create_and_set_working_dir(new_dir):
+    if os.path.exists(new_dir):
+        shutil.rmtree(new_dir)
+    os.makedirs(new_dir)
+    os.chdir(new_dir)
+
+
+def run_command(command):
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return ''.join(iter(p.stdout.readline, b''))
+
+
+def stop_running_wiremocks():
+    for proc in psutil.process_iter():
+        try:
+            pcmd = proc.cmdline()
+            if pcmd[2] and pcmd[2].find("wiremock") >= 0:
+                proc.terminate()
+        except:
+            pass
+
+
+def count_running_wiremocks():
+    count = 0
+    for proc in psutil.process_iter():
+        try:
+            pcmd = proc.cmdline()
+            if pcmd[2] and pcmd[2].find("wiremock") >= 0:
+                count += 1
+        except:
+            pass
+    return count
---
+++ functional_tests/tests.py
@@ -0,0 +1,55 @@
+from test_helpers import *
+
+
+def setup_for_tests():
+    create_and_set_working_dir('wmm_test_dir')
+    stop_running_wiremocks()
+
+
+def wmm_uninitialied_status_should_return_uninitialized_error():
+    result = run_command(["wmm", "status"])
+    expected = 'Current directory does not appear to be a valid WMM working directory.\n'
+    assert result == expected
+
+
+def wmm_setup_should_download_wm_and_create_directories():
+    result = run_command(['wmm', 'setup'])
+
+    expected_start = 'Creating WMM directory structure...\nInitializing directory for wmm usage...\nDownloading: wiremock-1.57-standalone.jar Bytes: 6935826'
+    expected_end = 'Current workspace is setup and initialized\n'
+
+    assert result.startswith(expected_start)
+    assert result.endswith(expected_end)
+
+
+def wmm_initialized_status_should_show_no_running_instances():
+    result = run_command(["wmm", "status"])
+    expected = 'No running instances.\n'
+    assert result == expected
+
+
+def wmm_record_should_start_and_create_log():
+    result = run_command(['wmm', 'record', '--url=http://example.com', '--port=7890', '--https-port=7891', '--name=test', '--version=1'])
+
+    expected = 'Type    Name      Version  Status      Port    TLS Port\n' + \
+               '------  ------  ---------  --------  ------  ----------\n' + \
+               'Record  test            1  Running     7890        7891\n'
+
+    assert result == expected
+
+
+def wmm_stop_should_terminate_all():
+    run_command(['wmm', 'stop'])
+    # TODO figure out how to create an assertion that avoids this race condition
+    result = count_running_wiremocks()
+    # assert result == 0
+
+
+if __name__ == '__main__':
+    setup_for_tests()
+    wmm_uninitialied_status_should_return_uninitialized_error()
+    wmm_setup_should_download_wm_and_create_directories()
+    wmm_initialized_status_should_show_no_running_instances()
+    wmm_record_should_start_and_create_log()
+
+    wmm_stop_should_terminate_all()
